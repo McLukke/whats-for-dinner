706b5bd78b2ae4222e49bbc02c54ebf0472bafac
Kenny Lu <Kenny@whynotkenny.com>
2018-02-11 13:31:15 -0500

    add custom tabs

--- login/index.js
+++ login/index.js
@@ -1,95 +1,41 @@
 import React from 'react';
-import cx from 'classnames';
 import { View, Text, StyleSheet } from 'react-native';
-import {
-  Button,
-  FormLabel,
-  FormInput,
-  FormValidationMessage
-} from 'react-native-elements';
-import { Actions } from 'react-native-router-flux';
+import Tabs from 'react-native-tabs';
 
-const initErrorState = {
-  username: false,
-  password: false,
-  message: 'This field is required'
-}
+import LoginForm from './loginForm';
+import SignupForm from './signupForm';
 
-const initState = {
-  username: '',
-  password: '',
-  errors: initErrorState
-}
-
-class LoginPage extends React.Component {
+export default class LoginPage extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = initState
-  }
-
-  updateInput = input => this.setState(input);
-
-  validate = () => {
-    const hasErrors = true;
-
-    // validate stuff
-
-    return [
-      hasErrors,
-      {
-        ...initErrorState,
-        username: true,
-        password: true
-      }
-    ];
-  }
-
-  handleLogin = () => {
-    const [hasErrors, errors] = this.validate();
-
-    if (hasErrors) {
-      return this.setState({ errors })
+    this.state = {
+      selectedTab: 'LoginForm'
     }
-
-    return Actions.home;
   }
 
   render() {
-    const errors = this.state.errors
+    const selectedTab = this.state.selectedTab;
 
     return (
       <View style={styles.container}>
-        <Text>Login Page</Text>
-
-        <View style={styles.form}>
-          <FormLabel>Username</FormLabel>
-          <FormInput onChange={e => this.updateInput({ username: e.target.value })} />
-          {errors.username ? <FormValidationMessage>{errors.message}</FormValidationMessage> : null}
-
-          <FormLabel>Password</FormLabel>
-          <FormInput onChange={e => this.updateInput({ password: e.target.value })} type='password' />
-          {errors.password ? <FormValidationMessage>{errors.message}</FormValidationMessage> : null}
-
-          <Button
-            title='Log In'
-            onPress={this.handleLogin}
-            textStyle={styles.btnTextStyle}
-            buttonStyle={styles.loginBtn}
-          />
+        <View style={styles.tabs}>
+          <Tabs
+            selected={selectedTab}
+            onSelect={e => this.setState({ selectedTab: e.props.name })}
+          >
+            <Text name='LoginForm'>Log In</Text>
+            <Text name='SignupForm'>Sign Up</Text>
+          </Tabs>
         </View>
+
+        {selectedTab === 'LoginForm'
+          ? <LoginForm />
+          : <SignupForm />}
       </View>
     );
   }
 }
-
-
-// <Button
-// title='Sign Up'
-// onPress={Actions.home}
-// textStyle={styles.btnTextStyle}
-// buttonStyle={cx(styles.btnsCommon, styles.signupBtn)}
-// />
 
 const styles = StyleSheet.create({
   container: {
@@ -97,19 +43,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  form: {
+  tabs: {
     width: '100%'
-  },
-  btnTextStyle: {
-    fontWeight: 'bold'
-  },
-  loginBtn: {
-    marginTop: 20,
-    backgroundColor: 'blue',
-    borderRadius: 30,
-    width: '60%',
-    alignSelf: 'center'
   }
 });
-
-export default LoginPage;
---
+++ login/signupForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class SignupForm extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Sign Up Form</Text>
+      </View>
+    );
+  }
+}

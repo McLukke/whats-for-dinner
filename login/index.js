1670ddacea8da729abd8478e8c0157f0cbb8e662
Kenny Lu <Kenny@whynotkenny.com>
2018-02-11 14:17:17 -0500

    tab styles

--- login/index.js
+++ login/index.js
@@ -5,14 +5,18 @@ import Tabs from 'react-native-tabs';
 import LoginForm from './loginForm';
 import SignupForm from './signupForm';
 
+const initState = {
+  selectedTab: 'LoginForm'
+}
+
 export default class LoginPage extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      selectedTab: 'LoginForm'
-    }
+    this.state = initState
   }
+
+  resetTabs = () => this.setState(initState)
 
   render() {
     const selectedTab = this.state.selectedTab;
@@ -24,14 +28,14 @@ export default class LoginPage extends React.Component {
             selected={selectedTab}
             onSelect={e => this.setState({ selectedTab: e.props.name })}
           >
-            <Text name='LoginForm'>Log In</Text>
-            <Text name='SignupForm'>Sign Up</Text>
+            <View selectedStyle={styles.selected} style={styles.tab} name='LoginForm'><Text>Log In</Text></View>
+            <View selectedStyle={styles.selected} style={styles.tab} name='SignupForm'><Text>Sign Up</Text></View>
           </Tabs>
         </View>
 
         {selectedTab === 'LoginForm'
-          ? <LoginForm />
-          : <SignupForm />}
+          ? <LoginForm resetTabs={this.resetTabs} />
+          : <SignupForm resetTabs={this.resetTabs} />}
       </View>
     );
   }
@@ -45,5 +49,20 @@ const styles = StyleSheet.create({
   },
   tabs: {
     width: '100%'
+  },
+  selected: {
+    borderBottomWidth: 5
+  },
+  tab: {
+    borderStyle: 'solid',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: 'blue',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
   }
 });
